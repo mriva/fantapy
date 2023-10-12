cb98a3265c8d560df8cb694fd0ec43f50bfb0360
Matteo Riva <muradin@gmail.com>
2023-10-12 17:55:20 +0200

    Add team data

---
+++ data.py
@@ -0,0 +1,27 @@
+altetico_medozzi_players = [
+    {"name": "Maignan", "role": "portiere", "score": 7},
+    {"name": "Theo", "role": "difensore", "score": 8},
+    {"name": "Tomori", "role": "difensore", "score": 7},
+    {"name": "Kjaer", "role": "difensore", "score": 6},
+    {"name": "Calabria", "role": "difensore", "score": 6},
+    {"name": "Reijnders", "role": "centrocampista", "score": 7},
+    {"name": "Adli", "role": "centrocampista", "score": 7},
+    {"name": "Loftus-Cheek", "role": "centrocampista", "score": 6},
+    {"name": "Leao", "role": "attaccante", "score": 8},
+    {"name": "Giroud", "role": "attaccante", "score": 10},
+    {"name": "Pulisic", "role": "attaccante", "score": 7},
+]
+
+virtus_secchi_players = [
+    {"name": "Provedel", "role": "portiere", "score": 6},
+    {"name": "Hysaj", "role": "difensore", "score": 5},
+    {"name": "Romagnoli", "role": "difensore", "score": 4},
+    {"name": "Casale", "role": "difensore", "score": 5},
+    {"name": "Marusic", "role": "difensore", "score": 5},
+    {"name": "Luis Alberto", "role": "centrocampista", "score": 4},
+    {"name": "Rovella", "role": "centrocampista", "score": 5},
+    {"name": "Guendouzi", "role": "centrocampista", "score": 6},
+    {"name": "Zaccagni", "role": "attaccante", "score": 5},
+    {"name": "Castellanos", "role": "attaccante", "score": 6},
+    {"name": "Felipe Anderson", "role": "attaccante", "score": 6},
+]
