a8daca2222d158e1fba625741807b1aeb167d754
Matteo Riva <muradin@gmail.com>
2023-11-04 10:40:08 +0100

    Nuovi esercizi da fare

---
+++ frequenzimetro.py
@@ -0,0 +1,5 @@
+"""
+Scrivi una funzione che, data una stringa come parametro, restituisca un dizionario rappresentante la "frequenza di comparsa" di ciascun carattere componente la stringa.
+
+Per fare un esempio, data una stringa "ababcc", otterremo in risultato {"a": 2, "b": 2, "c": 2}
+"""
---
+++ numero_perfetto.py
@@ -0,0 +1,4 @@
+"""
+Un numero perfetto è un numero naturale uguale alla somma dei suoi divisori positivi, escluso sé stesso.
+Scrivi una funzione che verifichi se un numero è perfetto oppure no.
+"""
